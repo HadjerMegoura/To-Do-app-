ef56d1fbde42bacefcef4cf595c49995f1930373
hadjer <hadjermegoura20@gmail.com>
2022-10-07 11:46:42 +0200

    first commit

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.forms import DateField
+from traitlets import default
 
 # Create your models here.
+class Note(models.Model):
+    text = models.CharField(max_length=30)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
